15627e60470eb806cf3b3a5dc24d92582115e48a
DanyangSu <fnosdy@gmail.com>
2019-04-20 01:54:14 -0400

    debug core/utils

--- linkalman/src/core/utils.py
+++ linkalman/src/core/utils.py
@@ -1,4 +1,4 @@
-from collections import Sequence
+from collections.abc import Sequence
 import numpy as np
 from scipy import linalg
 
@@ -19,7 +19,7 @@ class M_wrap(Sequence):
         self.L = None
         self.D = None
         self.L_I = None
-        self.pdet = None
+        self.m_pdet = None
         self.m_list = m_list
         
     def __getitem__(self, index):
@@ -28,10 +28,10 @@ class M_wrap(Sequence):
     def __len__(self):
         return len(self.m_list)
 
-    def _equal_M(self, t):
+    def _equal_M(self, index):
         """
-        Return true if self.m_list[t] == self.m. 
-        If false, set self.m = self.m_list[t]
+        Return true if self.m_list[index] == self.m. 
+        If false, set self.m = self.m_list[index]
         """
         if np.array_equal(self.m, self.m_list[index]):
             return True
@@ -43,7 +43,7 @@ class M_wrap(Sequence):
         """
         Return pseudo-inverse of self.m_list[index]
         """
-        if (not self._equal_M(index)) or self.m_pinv is None:
+        if (not self._equal_M(index)) or self.m_pinvh is None:
             self.m_pinvh = inv(self.m)
         return self.m_pinvh
     
@@ -51,9 +51,9 @@ class M_wrap(Sequence):
         """
         Calculate L and D from LDL decomposition, and inverse of L
         """
-        if (not self._equal_M(index)) or self.m_l is None:
+        if (not self._equal_M(index)) or self.L is None:
             self.L, self.D, _ = linalg.ldl(self.m)
-            self.L_I = linalg.lapack.clapack.dtrtri(self.L, lower=True)
+            self.L_I, _ = linalg.lapack.clapack.dtrtri(self.L, lower=True)
         return self.L, self.D, self.L_I
 
     def pdet(self, index):
@@ -64,5 +64,5 @@ class M_wrap(Sequence):
             eig = linalg.eigh(self.m, eigvals_only=True)
 
             # If all eigenvalues are close to 0, np.product(np.array([])) returns 1
-            self.pdet = np.product(eig[abs(eig)>1e-12])
-        return self.pdet
+            self.m_pdet = np.product(eig[np.abs(eig)>1e-12])
+        return self.m_pdet
