6080757810a328ba0f644549423efe77c495b3b7
DanyangSu <fnosdy@gmail.com>
2019-09-30 21:21:10 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,25 @@ if sys.version_info[:2] < (3, 5):
     raise RuntimeError("Python version >= 3.5 required.")
 
 setup(
-    name="linkalman", 
-    version='0.9.1',
-    author='Danyang Su', 
-    description='Flexible Linear Kalman Filter',
-    license='BSD 3',
-    packages=find_packages(),
-    install_requires=[
+    name = "linkalman", 
+    version = '0.11.2',
+    author = 'Danyang Su',
+    author_email = 'fnosdy@gmail.com',
+    url = 'https://github.com/DanyangSu/linkalman',
+    download_url = 'https://github.com/DanyangSu/linkalman/archive/v0.11.2.tar.gz',
+    description = 'Flexible Linear Kalman Filter',
+    keywords = ['kalman', 'time series', 'signal', 'filter'],
+    license = 'BSD 3',
+    packages = find_packages(),
+    install_requires = [
         'numpy',
         'scipy',
         'pandas',
         'networkx'
         ],
+    classifers = [
+        'Development Status :: 3 - Alpha',
+        'License :: BSD 3',
+        'Programming Language :: Python :: 3.7'
+        ]
     )
